f1994351763615540a622421174fe75b85726f2b
PGC <pgc@crt.com>
2026-08-26 09:26:13 +0200

    Add startup banner prestartup_script.py (v0.38.0)

---
+++ prestartup_script.py
@@ -0,0 +1,13 @@
+"""OpenBlender-HyMotion-CRT startup banner."""
+
+from __future__ import annotations
+
+import sys
+
+print("", file=sys.stderr)
+print("\033[36m" r"  ____  ___  _____  _____  __   _____  _____  _______ " "\033[0m", file=sys.stderr)
+print("\033[36m" r" / __ \/ _ \/ __/ |/ / _ )/ /  / __/ |/ / _ \/ __/ _ \" "\033[0m", file=sys.stderr)
+print("\033[36m" r"/ /_/ / ___/ _//    / _  / /__/ _//    / // / _// , _/" "\033[0m", file=sys.stderr)
+print("\033[36m" r"\____/_/  /___/_/|_/____/____/___/_/|_/____/___/_/|_|  " "\033[0m", file=sys.stderr)
+print(r"                                               " "\033[93mv0.38.0\033[0m", file=sys.stderr)
+print("", file=sys.stderr)
